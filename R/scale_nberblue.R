0f0dc0eeca71245177ed335ac1aff5cc120c7f80
weiyangtham <weiyang.tham@gmail.com>
2017-05-23 23:41:14 -0400

    simple scales

--- R/scale_nberblue.R
+++ R/scale_nberblue.R
@@ -1,2 +1,15 @@
 # NBER blue shades
 # "#0079C2" "#7AABDB"
+
+nber_pal <- c("#0079C2", "#7AABDB")
+
+scale_color_nber <- function(){
+  scale_colour_manual(values = nber_pal)
+}
+
+scale_colour_nber <- scale_color_stata
+
+scale_fill_nber <- function(){
+  scale_fill_manual(values = nber_pal)
+}
+
