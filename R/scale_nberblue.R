dc86fe22cfe1a263876dfcebb16507ac288e6b95
weiyangtham <weiyang.tham@gmail.com>
2017-05-23 23:49:46 -0400

    warning against monochrome palette

--- R/scale_nberblue.R
+++ R/scale_nberblue.R
@@ -3,13 +3,18 @@
 
 nber_pal <- c("#0079C2", "#7AABDB")
 
+pal_message = c("theme_nber: This scale only supports 2 values. If you have more than 2 values,
+          I recommend against using the NBER's usual monochrome palette")
+
 scale_color_nber <- function(){
+  message(pal_message)
   scale_colour_manual(values = nber_pal)
 }
 
-scale_colour_nber <- scale_color_stata
+scale_colour_nber <- scale_color_nber
 
 scale_fill_nber <- function(){
+  message(pal_message)
   scale_fill_manual(values = nber_pal)
 }
 
