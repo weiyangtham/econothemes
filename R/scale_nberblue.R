fc91b6d3e0c92007da4682a928a3abc301af3461
weiyangtham <weiyang.tham@gmail.com>
2017-06-01 23:31:56 -0400

    corrected scales functions

--- R/scale_nberblue.R
+++ R/scale_nberblue.R
@@ -1,20 +1,21 @@
-# NBER blue shades
-# "#0079C2" "#7AABDB"
+#' NBER blue shades
 
-nber_pal <- c("#0079C2", "#7AABDB")
+nber_palette = c("#0079C2", "#7AABDB")
+
+nber_pal <- function() { scales::manual_pal(nber_palette) }
 
 pal_message = c("theme_nber: This scale only supports 2 values. If you have more than 2 values,
           I recommend against using the NBER's usual monochrome palette")
 
-scale_color_nber <- function(){
+scale_color_nber <- function(...){
   message(pal_message)
-  scale_colour_manual(values = nber_pal)
-}
+  discrete_scale("colour", "nber", nber_pal(), ...)
+  }
 
 scale_colour_nber <- scale_color_nber
 
 scale_fill_nber <- function(){
   message(pal_message)
-  scale_fill_manual(values = nber_pal)
+  discrete_scale("colour", "nber", nber_pal(), ...)
 }
 
