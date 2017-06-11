2d8d75bb281ed370b27bbfb66a229629c5781552
weiyangtham <weiyang.tham@gmail.com>
2017-06-11 18:12:52 -0400

    first try at documentation

--- R/scale_nberblue.R
+++ R/scale_nberblue.R
@@ -1,21 +1,34 @@
-#' NBER blue shades
+nber_palette <- c("#0079C2", "#7AABDB")
 
-nber_palette = c("#0079C2", "#7AABDB")
+#' NBER blue shades
+#'
+#' Color palette with 2 shades of blue if you want to be consistent with NBER's style.
+#' @export
 
 nber_pal <- function() { scales::manual_pal(nber_palette) }
 
-# pal_message = c("theme_nber: This scale only supports 2 values. If you have more than 2 values,
-          # I recommend against using the NBER's usual monochrome palette")
+#' Discrete color scale based on the NBER palette
+#'
+#' @inheritParams ggplot2::scale_colour_hue
+#'
+#' @return None
+#'
+#' @export
+#' @rdname scale_nber
 
 scale_color_nber <- function(...){
-  # message(pal_message)
   discrete_scale("colour", "nber", nber_pal(), ...)
   }
 
+#' @export
+#' @rdname scale_nber
+
 scale_colour_nber <- scale_color_nber
 
-scale_fill_nber <- function(){
-  # message(pal_message)
+#' @export
+#' @rdname scale_nber
+
+scale_fill_nber <- function(...){
   discrete_scale("colour", "nber", nber_pal(), ...)
 }
 
